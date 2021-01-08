5daf0e8e390088a4cd1b15040993e5a7ab9903e1
ewnd9 <ewndnine@gmail.com>
2021-01-08 21:33:00 +0300

    add docker-build.yml

---
+++ src/ci.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const execa = require('execa');
+const { Readable } = require('stream');
+
+const fs = require('fs');
+const { run } = require('./docker');
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+async function main() {
+  await dockerLogin();
+  await dockerInitExperimental();
+
+  for (const input of fs.readdirSync('docker')) {
+    const image = `${process.env.DOCKER_REGISTRY_IMAGE}:${input}`;
+    console.log({ input, image });
+
+    const isExists = await dockerIsImageExists(image);
+    if (isExists) {
+      continue;
+    }
+
+    const isSuccess = await run({ input, image });
+
+    if (isSuccess) {
+      await execa(`docker push ${image}`, { shell: true, stdio: 'inherit' });
+    }
+  }
+}
+
+async function dockerLogin() {
+  // https://github.com/feuertiger/feuertiger/blob/cf79a11413118465e66d4a7ac822475ed579bbec/cli/src/dockerize.ts
+  const dockerLoginProcess = execa(
+    `docker login ghcr.io --username ${process.env.DOCKER_REGISTRY_USERNAME} --password-stdin`,
+    {
+      shell: true,
+      stdout: 'inherit',
+      stderr: 'inherit',
+    }
+  );
+
+  Readable.from([process.env.DOCKER_REGISTRY_PASSWORD]).pipe(
+    dockerLoginProcess.stdin
+  );
+
+  await dockerLoginProcess;
+}
+
+async function dockerInitExperimental() {
+  const configPath = `${process.env.HOME}/.docker/config.json`;
+  const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+  config.experimental = 'enabled';
+  fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
+}
+
+async function dockerIsImageExists(image) {
+  try {
+    await execa(`docker manifest inspect ${image}`, {
+      shell: true,
+      stdio: 'inherit',
+    });
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+}
--- src/test.js
+++ src/test.js
@@ -1,14 +1,6 @@
 'use strict';
 
-const execa = require('execa');
-const { remote } = require('webdriverio');
-
-const {
-  LOG_LEVEL = 'error',
-  CHROMEDRIVER_HOST = 'localhost',
-  CHROMEDRIVER_PORT = '9515',
-  TEST_URL = 'chrome://version',
-} = process.env;
+const { run } = require('./docker');
 
 main().catch((err) => {
   console.error(err);
@@ -17,82 +9,6 @@ main().catch((err) => {
 
 async function main() {
   const input = process.argv[2];
-  const useDocker = input !== 'existing';
-
-  async function fn() {
-    const text = await runTest();
-    console.log({ text });
-  }
-
-  if (useDocker) {
-    await runWithDocker(fn, input);
-  } else {
-    await fn();
-  }
-}
-
-async function runWithDocker(fn, input) {
-  const data = require('../data.json');
-  const result = Object.entries(data.versions).find(
-    ([version]) => version === input
-  );
-
-  if (!result) {
-    console.error(`no ${input}`);
-    process.exit(1);
-  }
-
-  const cwd = `docker/${result[0]}`;
-  const exec = async (cmd) =>
-    execa(cmd, {
-      shell: true,
-      cwd,
-    });
-
-  await exec('docker build -t test .');
-
-  // unclear how to run without --privileged (https://github.com/jessfraz/dockerfiles/issues/350)
-  // chrome 60+ doesn't require it
-  const { stdout: containerId } = await exec(
-    'docker run --privileged -d -p 9515:9515 test'
-  );
-
-  console.log({ containerId });
-
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  try {
-    await fn();
-  } catch (err) {
-    console.log(err);
-  }
-
-  await exec(`docker stop ${containerId} && docker rm ${containerId}`);
-}
-
-async function runTest() {
-  const browser = await remote({
-    logLevel: LOG_LEVEL,
-    hostname: CHROMEDRIVER_HOST,
-    port: CHROMEDRIVER_PORT ? Number(CHROMEDRIVER_PORT) : undefined,
-    path: '/',
-    capabilities: {
-      browserName: 'chrome',
-      'goog:chromeOptions': {
-        args: ['--no-sandbox'],
-      },
-    },
-  });
-
-  await browser.url(TEST_URL);
-
-  // @TODO: use wait in webdriverio
-  // await new Promise((resolve) => setTimeout(resolve, 10000));
-  await browser.saveScreenshot('sc3.png');
-  const text = await browser.execute(function () {
-    return document.querySelector('#version').innerText;
-  });
-  await browser.deleteSession();
-
-  return text;
+  const image = 'test';
+  await run({ input, image });
 }
